0091912379efec236f483240d4159c7665d9b19c
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-09-30 19:57:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,25 +5,7 @@ const config = require("./config.json");
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
  // client.user.setActivity(`Serving ${client.guilds.size} servers`);
-  client.user.setActivity("$(client.guilds.size) servers", {
-      type: "WATCHING",
-      url: "https://www.twitch.tv"
-  });
-});
-
-client.on("guildCreate", guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-//  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-  client.user.setActivity("$(client.guilds.size) servers", {
-      type: "WATCHING",
-      url: "https://www.twitch.tv"
-  });
-});
-
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
- // client.user.setActivity(`Serving ${client.guilds.size} servers`);
-  client.user.setActivity("$(client.guilds.size) servers", {
+  client.user.setActivity(`Pokémon`, {
       type: "WATCHING",
       url: "https://www.twitch.tv"
   });
