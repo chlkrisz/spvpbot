a4fbfb3b1a51b086c46145487f7736cf5a6d281b
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-09-30 21:28:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,10 +27,53 @@ if(command === "generate") {
  }
 
 if(command === "tre") {
-    message.channel.send({embed: {
-     color: 3447003,
-     description: "A very simple Embed!"
-    }});
+const embed = {
+  "title": "title ~~(did you know you can have markdown here too?)~~",
+  "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+  "url": "https://discordapp.com",
+  "color": 6500106,
+  "timestamp": "2018-09-30T19:24:27.750Z",
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+    "text": "footer text"
+  },
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "image": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "author": {
+    "name": "author name",
+    "url": "https://discordapp.com",
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "fields": [
+    {
+      "name": "🤔",
+      "value": "some of these properties have certain limits..."
+    },
+    {
+      "name": "😱",
+      "value": "try exceeding some of them!"
+    },
+    {
+      "name": "🙄",
+      "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "these last two",
+      "inline": true
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "are inline fields",
+      "inline": true
+    }
+  ]
+};
+channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed });
   
 }
   
