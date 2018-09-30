30db24c732ee17e92f4a84b1771f3e1bb0d6a706
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-09-30 20:29:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const config = require("./config.json");
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
  // client.user.setActivity(`Serving ${client.guilds.size} servers`);
-  client.user.setActivity("asd", {
-      type: "DND"
-  });
+    client.user.setStatus('dnd', 'say !help') 
 });
 
 function altgen() {
