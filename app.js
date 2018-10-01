4da2c94d58c20c72bf97b00a2df238f4661f6314
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-10-01 17:13:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
  // client.user.setActivity(`Serving ${client.guilds.size} servers`);
     client.user.setStatus('idle', '');
-    client.user.setActivity("say !help | W.I.P.", {
+    client.user.setActivity(`Serving ${client.guilds.size} servers`, {
      type: "STREAMING",
      url: "https://www.twitch.tv/chlkrisz"
     });
