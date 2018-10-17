8e8638270087c9843b3eff66a2716929834d543b
hungaryck <39501471+hungaryck@users.noreply.github.com>
2018-10-17 20:49:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,15 @@ client.on("message", async message => {
 
   if(message.author.bot) return;
   
+    // Grab the command data from the client.commands Enmap
+  const cmd = client.commands.get(command);
+
+  // If that command doesn't exist, silently exit and do nothing
+  if (!cmd) return;
+
+  // Run the command
+  cmd.run(client, message, args);
+  
   if(message.includes === "ogreszem") {
      message.delete().catch();
   }
