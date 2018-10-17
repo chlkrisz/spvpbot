2a2e7fc75ed0daf23a7c9274766c594b4742fbd3
hungaryck <39501471+hungaryck@users.noreply.github.com>
2018-10-17 19:02:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const config = require("./config.json");
 // Hey! This is MY BOT! Please DON'T STOLE MY BOT'S TOKEN! Thanks! :) //
 
 const fs = require("fs");
-client.commands = new Discord.Collection();
+bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
   if(err) console.error(err);
@@ -18,7 +18,7 @@ fs.readdir("./commands/", (err, files) => {
   jsfiles.forEach((f, i) => {
     let props = require(`./commands/$(f)`);
     console.log(`${i + 1}: ${f} loaded!`);
-    client.commands.set(props.help.name, props); 
+    bot.commands.set(props.help.name, props); 
   });
 });
 
